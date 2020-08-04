429415b20286ff223b4a7a40e4ecf2d60ae9f03f
Jim Mussared <jim.mussared@gmail.com>
2020-08-04 13:47:04 +1000

    light: Make time-aware also bright-in-the-morning.

--- hazard/things/light.py
+++ hazard/things/light.py
@@ -64,8 +64,10 @@ class Light(Thing):
       else:
         await self.level(level=0.05, onoff=True, soft=soft)
     else:
-      if time_aware and (datetime.datetime.now().hour >= 18 or datetime.datetime.now().hour < 8):
+      if time_aware and (datetime.datetime.now().hour >= 18 or datetime.datetime.now().hour <= 6):
         await self.level(level=0.05, onoff=True, soft=soft)
+      elif time_aware and (datetime.datetime.now().hour >= 7 or datetime.datetime.now().hour <= 11):
+        await self.level(level=1, onoff=True, soft=soft)
       else:
         await self.on(soft=soft)
 
